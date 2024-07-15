286acf2f0e5c2c2c085847b3d8f204cf361f8e7f
Yulia <160648656+yuuliasha@users.noreply.github.com>
2024-07-15 16:17:54 +0300

    Задачи.sql

--- Задачи.sql
+++ Задачи.sql
@@ -288,7 +288,31 @@ ORDER BY last_name
 	
 -- 44. Найдите максимальный возраст (количество лет) среди обучающихся 10 классов на сегодняшний день. 
        Для получения текущих даты и времени используйте функцию NOW().
+SELECT FLOOR(MAX((DATEDIFF(NOW(), birthday)/365))) AS max_year 
+FROM Student JOIN Student_in_class ON Student.id=Student_in_class.student 
+JOIN Class ON Class.id=Student_in_class.class 
+WHERE Class.name LIKE '10%'
+
+Второй вариант:
+SELECT TIMESTAMPDIFF(YEAR, birthday, CURRENT_TIMESTAMP) AS max_year
+FROM Student st
+         JOIN Student_in_class sc ON sc.student = st.id
+         JOIN Class cl ON cl.id = sc.class
+WHERE name LIKE '10 %'
+ORDER BY max_year DESC
+LIMIT 1
+
 -- 45. Какие кабинеты чаще всего использовались для проведения занятий? Выведите те, которые использовались максимальное количество раз.
+SELECT classroom
+FROM Schedule
+GROUP BY classroom
+HAVING count(classroom) = (
+    SELECT COUNT(*) AS count
+    FROM Schedule
+    GROUP BY classroom
+    ORDER BY count DESC
+    LIMIT 1
+);
 -- 46. В каких классах введет занятия преподаватель "Krauze" ?
 SELECT DISTINCT name 
 FROM Class 
@@ -331,7 +355,6 @@ ORDER BY count DESC
 
 -- 49. Какой процент обучающихся учится в "10 A" классе? 
        Выведите ответ в диапазоне от 0 до 100 с округлением до четырёх знаков после запятой, например, 96.0201.
-
 SELECT (COUNT(*)*100/(SELECT COUNT(Student.id) as count 
 FROM Student 
 JOIN Student_in_class ON Student.id=Student_in_class.student)) AS percent 
@@ -378,6 +401,28 @@ FROM FamilyMembers
 WHERE member_name LIKE '%Quincey'
 	
 -- 55. Удалить компании, совершившие наименьшее количество рейсов.
+DELETE
+FROM company
+WHERE id IN (
+    SELECT company
+    FROM trip
+    GROUP BY company
+    HAVING COUNT(*) = (
+        SELECT COUNT(*) AS count
+        FROM trip
+        GROUP BY company
+        ORDER BY count
+        LIMIT 1
+    )
+);
+Второй вариант:
+SELECT name, COUNT(company) as company 
+FROM Trip JOIN Company ON Company.id=Trip.company 
+GROUP BY name; DELETE FROM Company WHERE id = 4; 
+DELETE FROM Company 
+WHERE id = 3; DELETE 
+FROM Company 
+WHERE id = 2
 	
 -- 56. Удалить все перелеты, совершенные из Москвы (Moscow).
 DELETE 
@@ -396,35 +441,156 @@ SET start_pair = ADDTIME(start_pair, '00:30:00'),
     end_pair   = ADDTIME(end_pair, '00:30:00');
 
 -- 58. Добавить отзыв с рейтингом 5 на жилье, находящиеся по адресу "11218, Friel Place, New York", от имени "George Clooney"
+INSERT INTO Reviews
+SET id             = (
+    SELECT COUNT(*) + 1
+    FROM Reviews rw
+),
+    reservation_id = (
+        SELECT rs.id
+        FROM Reservations rs
+                 JOIN Rooms rm ON rm.id = rs.room_id
+                 JOIN Users us ON rs.user_id = us.id
+        WHERE address = '11218, Friel Place, New York'
+          AND name = 'George Clooney'
+    ),
+    rating         = 5;
+
 -- 59. Вывести пользователей,указавших Белорусский номер телефона ? Телефонный код Белоруссии +375.
+SELECT * 
+FROM Users 
+WHERE phone_number LIKE '+375%'
+
 -- 60. Выведите идентификаторы преподавателей, которые хотя бы один раз за всё время преподавали в каждом из одиннадцатых классов.
+SELECT teacher
+FROM Schedule sc
+JOIN Class cl ON sc.class = cl.id
+WHERE name LIKE '11 %'
+GROUP BY teacher
+HAVING COUNT(DISTINCT name) = 2;
+
 -- 61. Выведите список комнат, которые были зарезервированы хотя бы на одни сутки в 12-ую неделю 2020 года. 
        В данной задаче в качестве одной недели примите период из семи дней, первый из которых начинается 1 января 2020 года. 
        Например, первая неделя года — 1–7 января, а третья — 15–21 января.
+
+SELECT Rooms.* 
+FROM Rooms 
+JOIN Reservations ON Rooms.id=Reservations.room_id AND YEAR(start_date)=2020 AND YEAR(end_date)=2020 
+WHERE WEEK(start_date, 1)=12 OR WEEK(end_date, 1)=12
+
+Второй вариант:
+SELECT Rooms.*
+FROM Reservations
+         JOIN Rooms ON Rooms.id = Reservations.room_id
+WHERE WEEK(start_date, 1) = 12
+  AND YEAR(start_date) = 2020;
+
 -- 62. Вывести в порядке убывания популярности доменные имена 2-го уровня, используемые пользователями для электронной почты. 
        Полученный результат необходимо дополнительно отсортировать по возрастанию названий доменных имён.
+SELECT SUBSTRING_INDEX(email, '@', -1) as domain, count(*) AS count 
+FROM Users GROUP BY domain 
+ORDER BY count DESC, domain ASC
+
 -- 63. Выведите отсортированный список (по возрастанию) фамилий и имен студентов в виде Фамилия.И.
+SELECT CONCAT(last_name, '.', LEFT(first_name, 1), '.') AS name
+FROM Student
+ORDER BY name
+
 -- 64. Вывести количество бронирований по каждому месяцу каждого года, в которых было хотя бы 1 бронирование. 
        Результат отсортируйте в порядке возрастания даты бронирования.
+SELECT YEAR(start_date)  AS year,
+       MONTH(start_date) AS month,
+       COUNT(*)          AS amount
+FROM Reservations
+GROUP BY YEAR(start_date),
+         MONTH(start_date)
+ORDER BY year, month
+
 -- 65. Необходимо вывести рейтинг для комнат, которые хоть раз арендовали, как среднее значение рейтинга отзывов округленное до целого вниз.
+SELECT room_id, FLOOR(AVG(rating)) AS rating 
+FROM Reservations JOIN Reviews ON Reviews.reservation_id=Reservations.id 
+GROUP BY room_id
 -- 66. Вывести список комнат со всеми удобствами (наличие ТВ, интернета, кухни и кондиционера), 
        а также общее количество дней и сумму за все дни аренды каждой из таких комнат.
+SELECT home_type, address, COALESCE(SUM(DATEDIFF(end_date, start_date)), 0) as days, COALESCE(SUM(Reservations.total), 0) AS total_fee 
+FROM Reservations RIGHT JOIN Rooms ON Rooms.id=Reservations.room_id 
+WHERE has_tv !=0 AND has_internet !=0 AND has_kitchen !=0 AND has_air_con !=0 
+GROUP BY address, home_type
+
 -- 67. Вывести время отлета и время прилета для каждого перелета в формате "ЧЧ:ММ, ДД.ММ - ЧЧ:ММ, ДД.ММ", 
        где часы и минуты с ведущим нулем, а день и месяц без.
+SELECT CONCAT(
+               DATE_FORMAT(time_out, '%H:%i, %e.%c'),
+               ' - ',
+               DATE_FORMAT(time_in, '%H:%i, %e.%c')
+           ) AS flight_time
+FROM Trip
+
 -- 68. Для каждой комнаты, которую снимали как минимум 1 раз, найдите имя человека, снимавшего ее последний раз, и дату, когда он выехал
+SELECT rs.room_id,
+       name,
+       date AS end_date
+FROM (
+         SELECT room_id,
+                MAX(end_date) AS date
+         FROM Reservations
+         GROUP BY room_id
+     ) rs
+         JOIN Reservations rsv ON rs.room_id = rsv.room_id
+    AND rs.date = rsv.end_date
+         JOIN Users us ON rsv.user_id = us.id;
 -- 69. Вывести идентификаторы всех владельцев комнат, что размещены на сервисе бронирования жилья и сумму, которую они заработали
+SELECT owner_id,
+       IFNULL(SUM(total), 0) AS total_earn
+FROM Rooms rm
+         LEFT JOIN Reservations rs ON rm.id = rs.room_id
+GROUP BY owner_id;
 -- 70. Необходимо категоризовать жилье на economy, comfort, premium по цене соответственно <= 100, 100 < цена < 200, >= 200. 
        В качестве результата вывести таблицу с названием категории и количеством жилья, попадающего в данную категорию
+SELECT 
+CASE
+WHEN price <= 100 THEN 'economy'
+WHEN price > 100 AND price < 200 THEN 'comfort'
+WHEN price >= 200 THEN 'premium'
+END  AS category,
+COUNT(price) AS count
+FROM Rooms
+GROUP BY category;
+
 -- 71. Найдите какой процент пользователей, зарегистрированных на сервисе бронирования, хоть раз арендовали или сдавали в аренду жилье. 
        Результат округлите до сотых.
+SELECT ROUND( (SELECT COUNT(*) 
+FROM (SELECT DISTINCT owner_id
+FROM Rooms rm
+JOIN Reservations rs ON rm.id = rs.room_id
+UNION
+SELECT user_id
+FROM Reservations) active_users) * 100 / (
+                           SELECT COUNT(*)
+                           FROM Users
+                       ), 2
+           ) AS percent;
 -- 72. Выведите среднюю цену бронирования за сутки для каждой из комнат, которую бронировали хотя бы один раз. 
        Среднюю цену необходимо округлить до целого значения вверх.
+SELECT room_id,
+       CEILING(AVG(price)) AS avg_price
+FROM Reservations
+GROUP BY room_id
+
 -- 73. Выведите id тех комнат, которые арендовали нечетное количество раз
+SELECT room_id,
+       COUNT(*) AS count
+FROM Reservations
+GROUP BY room_id
+HAVING count % 2 != 0
+
 -- 74. Выведите идентификатор и признак наличия интернета в помещении. 
 Если интернет в сдаваемом жилье присутствует, то выведите «YES», иначе «NO».
+
 SELECT id,
 IF(has_internet = 1, 'YES', 'NO') AS has_internet
 FROM Rooms;
+
 -- 75. Выведите фамилию, имя и дату рождения студентов, кто был рожден в мае.
 SELECT last_name, first_name, birthday
 FROM Student
@@ -433,17 +599,82 @@ WHERE MONTHNAME(birthday) = 'May'
 -- 76. Вывести имена всех пользователей сервиса бронирования жилья, 
        а также два признака: является ли пользователь собственником какого-либо жилья (is_owner) и является ли пользователь арендатором (is_tenant). 
        В случае наличия у пользователя признака необходимо вывести в соответствующее поле 1, иначе 0.
+SELECT name,
+       IF(
+                   id IN (
+                   SELECT owner_id
+                   FROM Rooms
+               ),
+                   1,
+                   0
+           ) AS is_owner,
+       IF(
+                   id IN (
+                   SELECT user_id
+                   FROM Reservations
+               ),
+                   1,
+                   0
+           ) AS is_tenant
+FROM Users;
+
 -- 77. Создайте представление с именем "People", которое будет содержать список имен (first_name) и фамилий (last_name) всех студентов (Student) 
        и преподавателей(Teacher)
+CREATE VIEW People AS
+SELECT first_name,
+       last_name
+FROM Student
+UNION
+SELECT first_name,
+       last_name
+FROM Teacher;
 -- 78. Выведите всех пользователей с электронной почтой в «hotmail.com»
+SELECT *
+FROM Users
+WHERE email RLIKE '@hotmail.com'
+
 -- 79. Выведите поля id, home_type, price у всего жилья из таблицы Rooms. 
        Если комната имеет телевизор и интернет одновременно, то в качестве цены в поле price выведите цену, применив скидку 10%.
+SELECT id,
+       home_type,
+       IF(has_tv AND has_internet, price * 0.9, price) AS price
+FROM Rooms
+
 -- 80. Создайте представление «Verified_Users» с полями id, name и email, которое будет показывает только тех пользователей, 
        у которых подтвержден адрес электронной почты.
+CREATE VIEW Verified_Users AS
+SELECT id, name, email
+FROM Users
+WHERE email_verified_at IS NOT NULL
+
 -- 93. Какой средний возраст клиентов, купивших Smartwatch (использовать наименование товара product.name) в 2024 году?
+SELECT AVG(uniqueCustomers.age) AS average_age
+FROM ( SELECT DISTINCT Customer.customer_key, Customer.age
+           FROM Purchase
+		JOIN Customer ON Purchase.customer_key = Customer.customer_key
+		JOIN Product ON Purchase.product_key = Product.product_key
+		WHERE Product.name = 'Smartwatch'
+			AND YEAR(Purchase.date) = 2024) AS uniqueCustomers
 -- 94. Вывести имена покупателей, каждый из которых приобрёл Laptop и Monitor (использовать наименование товара product.name) в марте 2024 года?
+SELECT Customer.name
+FROM Customer
+JOIN Purchase ON Customer.customer_key = Purchase.customer_key
+JOIN Product ON Purchase.product_key = Product.product_key
+WHERE Product.name IN ('Laptop', 'Monitor')
+	AND MONTH(Purchase.date) = 3
+	AND YEAR(Purchase.date) = 2024
+GROUP BY Customer.customer_key
+HAVING COUNT(DISTINCT Product.name) = 2
+
 -- 97. Посчитать количество работающих складов на текущую дату по каждому городу. 
        Вывести только те города, у которых количество складов более 80. Данные на выходе - город, количество складов.
+SELECT city, COUNT(warehouse_id) AS warehouse_count
+FROM Warehouses
+WHERE date_close IS NULL -- Выбираем склады, у которых дата закрытия отсутствует, т.е. работающие
+GROUP BY city
+HAVING COUNT(warehouse_id) > 80 -- Фильтруем результат, оставляя только города с количеством складов больше 80
+ORDER BY city; -- Сортируем по городу
+
 -- 99. Посчитай доход с женской аудитории (доход = сумма(price * items)). 
        Обратите внимание, что в таблице женская аудитория имеет поле user_gender «female» или «f».
 SELECT SUM(price * items) AS income_from_female
